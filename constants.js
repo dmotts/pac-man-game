78aea32011cc23f1905a3947f7afa1242265a4ab
blakknot <lil_spyguy@hotmail.com>
2023-05-07 21:00:01 -0400

    Add constants.js file

---
+++ constants.js
@@ -0,0 +1,16 @@
+const GRID_SIZE = 20;
+const CELL_SIZE = 20;
+const OBJECT_TYPE = {
+    BLANK: 'blank',
+    WALL: 'wall',
+    DOT: 'dot',
+    ENERGIZER: 'energizer',
+    GHOST: 'ghost',
+    PACMAN: 'pacman',
+};
+
+module.exports = {
+    GRID_SIZE,
+    CELL_SIZE,
+    OBJECT_TYPE,
+};
